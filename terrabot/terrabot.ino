ce784e9e1a2c5ece7d28109492a1aa007357f0a4
Jreyes1 <jr31@princeton.edu>
2018-08-02 16:31:45 -0400

    cleaned up blink code

--- terrabot/terrabot.ino
+++ terrabot/terrabot.ino
@@ -10,25 +10,24 @@
 // and subscibers and handles serial port communication
 ros::NodeHandle teensy;
 
-
 // callback function for the subsriber. Call back function must take constant
 // reference of a message as its argument. Type of message is std_msgs::Empy
 // and the message name will be toggle_msg
-void messageCb( const std_msgs::Empty& toggle_msg) {;
+void blinkCB( const std_msgs::Empty& toggle_msg) {;
   digitalWrite(13, HIGH-digitalRead(13));
 }
 
 
-
 // insantiate subsciber. template must be baced on the message.
 // two arguments are topic being subscribed to and callback function
-ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+ros::Subscriber<std_msgs::Empty> sub_led("toggle_led", &blinkCB);
 
 void setup() {
   pinMode(13, OUTPUT);
   teensy.initNode();
-  teensy.subscribe(sub);
 
+  // subsribe to topic
+  teensy.subscribe(sub_led);
 }
 
 void loop() {
